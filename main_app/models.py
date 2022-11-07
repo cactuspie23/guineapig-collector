f5824dfa828a0fb5b485ec5548f1df85e21384d9
AlyssaD <pink_lime@live.com>
2022-11-07 17:26:14 -0500

    add daily feedings tracking

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('H', 'Hay'),
@@ -21,6 +22,9 @@ class Guineapig(models.Model):
 
   def get_absolute_url(self):
     return reverse('guineapigs_detail', kwargs={'guineapig_id': self.id})
+
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
