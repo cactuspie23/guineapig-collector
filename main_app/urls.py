71e347da94adfa70ca0b735e8b443e00ee0e78df
AlyssaD <pink_lime@live.com>
2022-11-08 16:15:21 -0500

    add login functionality for homepage

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('guineapigs/', views.guineapigs_index, name='guineapigs_index'),
   path('guineapigs/<int:guineapig_id>/', views.guineapigs_detail, name='guineapigs_detail'),
