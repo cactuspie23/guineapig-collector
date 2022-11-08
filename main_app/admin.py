25e3dc1d8e7c5b2cc16bf6fe99827a409565888c
AlyssaD <pink_lime@live.com>
2022-11-07 19:25:42 -0500

    accessory admin and migrate

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Guineapig, Feeding
+from .models import Guineapig, Feeding, Accessory
 
 # Register your models here.
 admin.site.register(Guineapig)
 admin.site.register(Feeding)
+admin.site.register(Accessory)
